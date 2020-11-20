e131ee33c072b3eee5500129ef7a3994f7192efe
witchlad <dolearczyk95@gmail.com>
2020-11-20 11:25:16 +0100

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var button = document.getElementById("toggle-btn");
-var navbar = document.getElementById("navbar-list-content");
+let button = document.getElementById("toggle-btn");
+let navbar = document.getElementById("navbar-list-content");
 
-button.addEventListener("click",function(){
+const change =() => {
     navbar.classList.toggle("opened")
-})
-
-button.addEventListener("touchstart", function(){
-    navbar.classList.toggle("opened")
-})
+}
